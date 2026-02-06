3588f60e0d3d0d9bc23286f3822d77bc1a382ff0
Han LI <lihantju@163.com>
2026-02-06 08:52:41 +0800

    Update GameplayDetails.tsx

--- components/GameplayDetails.tsx
+++ components/GameplayDetails.tsx
@@ -5,7 +5,7 @@ const GameplayDetails: React.FC = () => {
   const [showGallery, setShowGallery] = useState(false);
 
   const cityImages = [
-    { src: "https://github.com/Sahara0304/web/blob/main/components/city1.png", title: "繁华中心区" },
+    { src: "city1.png", title: "繁华中心区" },
     { src: "https://github.com/Sahara0304/web/blob/main/components/city2.png", title: "立体交通网" },
     { src: "https://github.com/Sahara0304/web/blob/main/components/city3.png", title: "生态居住区" },
     { src: "https://github.com/Sahara0304/web/blob/main/components/city4.png", title: "城市夜景" },
